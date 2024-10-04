f42f3a0f1c6cb8313d3776169bbe6b66304fa86e
itsajay001 <133009231+itsajay001@users.noreply.github.com>
2024-10-04 20:53:06 +0530

    react router

---
+++ src/Components/Github/Github.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
+
+function Github() {
+  // const [data, setData]= useState([])
+  // useEffect(()=>{
+  //   fetch("https://api.github.com/users/itsajay001")
+  //   .then(res=>res.json())
+  //   .then (data=> setData(data))
+
+  // }, [])
+  const data = useLoaderData()
+
+  return (
+    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers: {data.followers}
+    <img src={data.avatar_url} alt="Git picture" width={300} />
+    </div>
+  )
+}
+
+export default Github
+
+export const githubLoader = async ()=>{
+  const responce = await fetch("https://api.github.com/users/itsajay001")
+  return responce.json()
+}
